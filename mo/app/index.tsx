cd66b0b4627a0cc0ab744d78f58c68a50ba86cc0
Architmaurya <architmaurya7054@gmail.com>
2025-06-26 17:02:58 +0530

    save the file

--- mo/app/index.tsx
+++ mo/app/index.tsx
@@ -1,9 +1,18 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-blue-500 text-4xl">Edit </Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-4xl font-bold text-gray-800">Welcome</Text>
+
+      <Link href="/onboarding" className="text-blue-600 mt-4 text-lg underline">
+        Go to Onboarding
+      </Link>
+
+      <Link href="/movie/avengers" className="text-blue-600 mt-2 text-lg underline">
+        View Avengers
+      </Link>
     </View>
   );
 }
